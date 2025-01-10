f798a89545b7d78db9616430f8503cd2d33ade4b
Separatehhh23 <132171290+Separatehhh23@users.noreply.github.com>
2025-01-10 18:05:08 +0100

    Update build.zig

--- build.zig
+++ build.zig
@@ -16,7 +16,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "zaig-web-framework",
+        .name = "zig-web-framework",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,

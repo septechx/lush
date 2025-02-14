ce83be8e3ff7eda911b567db57e518a627837e72
septech <septechx@proton.me>
2025-02-14 18:21:49 +0100

    Add docs to BundlerConfig

--- modules/cli/scripts/index.ts
+++ modules/cli/scripts/index.ts
@@ -1,3 +1,7 @@
 import type { BuildConfig } from "bun";
 
+/**
+ *  Default export in bundler.config.ts should be this type.
+ *  It's needed because by default bun's config requires an entrypoint, but a default is set elseware so it should be optional in this case
+ */
 export type BundlerConfig = Partial<BuildConfig>;

8faff6c197ea4b1bdf9522a7667a021fd20fdadd
septech <septechx@proton.me>
2025-02-20 00:12:47 +0100

    Add build output to .gitignore

--- build/package/README.md
+++
@@ -1,2 +0,0 @@
-# @lush/lush
-[![JSR](https://jsr.io/badges/@lush/lush)](https://jsr.io/@lush/lush)
--- build/package/scripts/bundler/config.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs/promises";
-import { BundlerConfig } from "..";
-
-export async function getConfig(path: string) {
-  const realPath = await fs.realpath(path);
-  return await import(realPath).then(
-    (d: { default: BundlerConfig }) => d.default,
-  );
-}
